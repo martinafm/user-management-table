d36c500755d32cf0faea33a0a9612e5d156444ef
Martyna <110538495+martinafm@users.noreply.github.com>
2024-09-27 18:30:09 +0200

    added missing attributes noopener and noreferrer to link, alt to img,

--- src/Header.tsx
+++ src/Header.tsx
@@ -7,9 +7,10 @@ const Header: React.FC = () => {
       <a
         href="https://github.com/martinafm"
         target="_blank"
+        rel="noopener noreferrer"
         className="inline-flex items-center justify-center self-start rounded-lg bg-gray-200 px-4 py-2 text-base font-medium text-gray-600 hover:bg-gray-300 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
       >
-        <img src={Arrow} width="20" height="20" className="mr-2" />
+        <img src={Arrow} width="20" height="20" className="mr-2" alt="Arrow"/>
         Go back to my GitHub
       </a>
       <h1 className="my-5 text-2xl font-medium sm:my-10 sm:text-5xl">
