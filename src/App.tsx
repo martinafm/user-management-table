bd8e2ae05624b5d6a05eecdc3c67b567901fded4
Martyna <110538495+martinafm@users.noreply.github.com>
2024-10-02 11:35:43 +0200

    converted App .jsx extension to .tsx , simplified App component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store/store.tsx';
+import UserManagementTable from './UserManagementTable.tsx';
+
+function App(): React.ReactNode {
+  return (
+    <Provider store={store}>
+      <UserManagementTable />
+    </Provider>
+  );
+}
+
+export default App;
