5e719f973b04e4a708392bec7880020632dc8110
Martyna <110538495+martinafm@users.noreply.github.com>
2024-10-02 12:14:23 +0200

    improved resource path file extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { store } from './store/store.tsx';
+import { store } from './store/store.ts';
 import UserManagementTable from './UserManagementTable.tsx';
 
 function App(): React.ReactNode {
