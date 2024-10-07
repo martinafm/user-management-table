0811c047f9835da7bce291ce443a93eead73ab66
Martyna <110538495+martinafm@users.noreply.github.com>
2024-10-07 11:41:20 +0200

    created svg component

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Arrow from '../assets/arrow.svg';
+import SvgArrow from './images/SvgArrow';
 
-const Header: React.FC = () => {
+const Header = () => {
   return (
     <header className="mx-4 my-8 flex w-full flex-col items-center justify-center font-header sm:my-16">
       <a
@@ -10,7 +10,7 @@ const Header: React.FC = () => {
         rel="noopener noreferrer"
         className="inline-flex items-center justify-center self-start rounded-lg bg-gray-200 px-4 py-2 text-base font-medium text-gray-600 hover:bg-gray-300 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
       >
-        <img src={Arrow} width="20" height="20" className="mr-2" alt="Arrow"/>
+        <SvgArrow/>
         Go back to my GitHub
       </a>
       <h1 className="my-5 text-2xl font-medium sm:my-10 sm:text-5xl">
